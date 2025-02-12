28c879f6e3aeee41b8af1ed9445ab665999d3061
chuanan.liu <chuanan.liu@sistalk.com>
2025-02-12 12:33:24 +0800

    add project

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<alipay_kit/AlipayKitPlugin.h>)
+#import <alipay_kit/AlipayKitPlugin.h>
+#else
+@import alipay_kit;
+#endif
+
+#if __has_include(<integration_test/IntegrationTestPlugin.h>)
+#import <integration_test/IntegrationTestPlugin.h>
+#else
+@import integration_test;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AlipayKitPlugin registerWithRegistrar:[registry registrarForPlugin:@"AlipayKitPlugin"]];
+  [IntegrationTestPlugin registerWithRegistrar:[registry registrarForPlugin:@"IntegrationTestPlugin"]];
+}
+
+@end
